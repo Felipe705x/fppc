7c77c43a336166b462c34993ee7c62649f8a229d
Felipe <favendano.cse@gmail.com>
2025-11-06 23:12:55 -0300

    refactor: fmt check

--- src/ast/mod.rs
+++ src/ast/mod.rs
@@ -5,7 +5,6 @@ mod pattern;
 mod types;
 mod var;
 
-
 pub use descriptor::{Descriptor, DescriptorType, PropertyType};
 pub use expr::{AttributeLookup, BinOpKind, Binop, Constant, Expr, UnOpKind, Unop};
 pub use label::LabelType;
