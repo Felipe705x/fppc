a6b1a794f99f4719b5bddf8b8491ce64b339c9da
Felipe705x <favendano.cse@gmail.com>
2026-01-11 16:40:29 -0300

    feat: edge AST defs

--- src/ast/pattern.rs
+++ src/ast/pattern.rs
@@ -6,11 +6,28 @@ use std::fmt;
 pub enum PathPattern {
     Node(Descriptor),
     Filter(Box<PathPattern>, Expr),
+    Edge(EdgeDirection, Descriptor),
 }
 
 impl PathPattern {
     pub fn filter(pattern: PathPattern, expr: Expr) -> Self {
         PathPattern::Filter(Box::new(pattern), expr)
+    }
+
+    pub fn edge(dir: EdgeDirection, desc: Descriptor, filter: Option<Expr>) -> Self {
+        let edge = PathPattern::Edge(dir, desc);
+        match filter {
+            None => edge,
+            Some(expr) => PathPattern::filter(edge, expr),
+        }
+    }
+
+    pub fn node(desc: Descriptor, filter: Option<Expr>) -> Self {
+        let node = PathPattern::Node(desc);
+        match filter {
+            None => node,
+            Some(expr) => PathPattern::filter(node, expr),
+        }
     }
 }
 
@@ -25,6 +42,15 @@ impl fmt::Debug for PathPattern {
         match self {
             PathPattern::Node(desc) => write!(f, "Node({:?})", desc),
             PathPattern::Filter(p, e) => write!(f, "Filter({:?}, {:?})", p, e),
+            PathPattern::Edge(dir, desc) => write!(f, "Edge({:?}, {:?})", dir, desc),
         }
     }
+}
+
+#[derive(PartialEq, Clone, Copy, Debug)]
+pub enum EdgeDirection {
+    Right,
+    Left,
+    None,
+    Any,
 }
